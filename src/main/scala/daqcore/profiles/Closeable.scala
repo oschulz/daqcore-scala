86ee87874f06a39ebb2b0e832818bb1d5b88db89
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-04-08 16:45:44 +0200

    Added profile.Closeable

---
+++ src/main/scala/daqcore/profiles/Closeable.scala
@@ -0,0 +1,42 @@
+// Copyright (C) 2010 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.devices
+
+import scala.actors._
+
+import daqcore.util._
+import daqcore.actors._
+
+
+trait Closeable extends ServerProxy {
+  profile[Closeable]
+
+  def close(): Unit =
+    { self ! Closeable.Close }
+    
+  def onClose(handler: () => Unit): Unit = { self ! Closeable.OnClose(handler) }
+}
+
+
+object Closeable {
+  case object Close
+
+  case class OnClose(handler: () => Unit)
+
+  def apply(a: Actor) = new Closeable { def self = a }
+}
