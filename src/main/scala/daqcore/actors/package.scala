d87f094bf56c87ebd9c92539d56af75b4265b054
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-04-10 00:47:35 +0200

    Added actors.actwait

--- src/main/scala/daqcore/actors/package.scala
+++ src/main/scala/daqcore/actors/package.scala
@@ -21,7 +21,28 @@ package daqcore
 package object actors {
   def profileOf[T <: ServerProxy : ClassManifest] =
     Profile.of[T]
+
+
+  /** execute code in an actor, then wait for it to exit.
+  Useful to execute code that relies on react magic (like Future.respond)
+  in a standard thread (e.g. the main thread). */
+  def actwait (body: => Unit) {
+    import scala.actors._, scala.actors.Actor._
     
+    case object Finished
+    val trapE = self.trapExit
+    self.trapExit = true
+    val a = new Actor { def act = body }
+    link(a)
+    a.start
+    receive {
+      case Exit(a, 'normal) =>
+      case Exit(a, reason) => exit(reason)
+    }
+    self.trapExit = trapE
+  }
+
+
   implicit def actorsFutureFix(actor: scala.actors.Actor) =
     new scala.actors.fix_daqcore.ActorsFutureFix(actor)
 }
