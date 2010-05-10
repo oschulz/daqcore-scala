8a9a60518aacf2d1b22123652426541397e1747e
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-05-10 11:33:56 +0200

    Added VMESCPIClient

---
+++ src/main/scala/daqcore/servers/VMESCPIClient.scala
@@ -0,0 +1,137 @@
+// Copyright (C) 2010 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.servers
+
+import scala.actors._
+
+import java.io.IOException
+import java.net.InetAddress
+import java.util.concurrent.TimeoutException
+
+import org.acplt.oncrpc.OncRpcProtocols
+
+import daqcore.oncrpc.vxi11core
+import daqcore.actors._
+import daqcore.profiles._
+import daqcore.util._
+
+import daqcore.prot.scpi._, daqcore.prot.scpi.mnemonics._
+
+
+class VMESCPIClient(dev: SCPIClientLink) extends Server with VMEBus {
+  val VME = Mnemonic("VME")
+  val defaultTimeout = 1000
+  
+  protected case class Fwd[T](target: OutputChannel[Any], op: T)
+  
+  class ReadQueue extends DaemonActor with Logging {
+    protected def unexpectedResponse: PartialFunction[Response, Unit] = { case resp =>
+      error("Unexpected SCPI response: " + resp)
+      exit('unexpectedResponse)
+    }
+  
+    protected def query(instr: Instruction*)(body: PartialFunction[Response, Unit]) = {
+      for (optRes <- dev.queryF(defaultTimeout, instr: _*)) {
+        optRes match {
+          case Some(response) => {
+            trace("Processing response: " + response)
+            (body orElse unexpectedResponse)(response)
+          }
+          case None => {
+            trace("Read timed out")
+            exit(Timeout)
+          }
+        }
+      }
+    }
+
+    def act() = {
+      link(dev.srv)
+      loop { react {
+        case op @ Fwd(repl, MemoryLink.Read(address, count)) => {
+          trace(op)
+          query(~VME~READ?(NR1(address.toInt), NR1(count.toInt))) {
+            case Response(Result(value)) => value match {
+              case BlockData(bytes) => {
+                trace("Read data: " + bytes)
+                repl ! bytes
+                trace("Replied read result to sender")
+              }
+              case value => {
+                error("Block Data expected: " + value.toList)
+                exit('unexpectedResponse)
+              }
+            }
+          }
+        }
+        case op @ Fwd(repl, VMEBus.GetBaseAddress()) => {
+          trace(op)
+          query(~VME~OFFSet?()) {
+            case Response(Result(NR1(address))) => {
+              trace("Base address: " + address)
+              trace("Replying to: " + repl.getClass)
+              repl ! address.toLong
+              trace("Replied base address to sender")
+            }
+          }
+        }
+        case _ => exit('unknownMessage)
+      } }
+    }
+  }
+  val readQueue = new ReadQueue
+
+  override def init() = {
+    link(dev.srv)
+    link(readQueue)
+    readQueue.start()
+  }
+
+
+  def serve = {
+    case op: MemoryLink.Read => {
+      trace(op)
+      readQueue.forward(Fwd(sender, op))
+    }
+    case op @ MemoryLink.Write(address, bytes) => {
+      trace(op)
+      dev.cmd(~VME~WRITe!(NR1(address.toInt), BlockData(bytes.toIndexedSeq)))  //!! toIndexedSeq not optimal
+    }
+    case op @ VMEBus.SetBaseAddress(address) => {
+      trace(op)
+      dev.cmd(~VME~OFFSet!(NR1(address.toInt)))
+    }
+    case op @ VMEBus.GetBaseAddress() => {
+      trace(op)
+      readQueue.forward(Fwd(sender, op))
+    }
+    case Closeable.Close => {
+      dev.close()
+      exit('closed)
+    }
+  }
+}
+
+
+object VMESCPIClient {
+  def apply (dev: SCPIClientLink) : VMESCPIClient =
+    start(new VMESCPIClient(dev))
+  
+  def apply (host: String, port: Int) : VMESCPIClient =
+    VMESCPIClient(SCPIClientLink(host, port))
+}
