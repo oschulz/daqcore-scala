a9be996d92dc07b303259037bf6cbed7bf09a986
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-05-16 16:17:59 +0200

    Added gui.Plot

---
+++ src/main/scala/daqcore/gui/Plot.scala
@@ -0,0 +1,153 @@
+// Copyright (C) 2010 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.gui
+
+import scala.swing._
+
+import org.jfree.chart.{ChartPanel => JFCPanel, JFreeChart => JFCChart, ChartFactory => JFCChartFactory }
+import org.jfree.chart.plot.{PlotOrientation => JFCPlotOrientation, XYPlot => JFCXYPlot}
+import org.jfree.data.{xy => jfcXY}
+import org.jfree.data.xy.{XYDataset => JFCXYDataset}
+import org.jfree.data.general.{Series => JFCSeries}
+
+
+abstract class Plot extends Proxy {
+  def self: JFCChart
+  def options: PlotOptions
+  
+  def draw(): Unit = {
+    val frame = PlotFrame(this)
+    frame.show()
+  }
+}
+
+
+
+abstract class PlotOptions {}
+
+
+
+case class XYPlotOptions (
+    title: String = "",
+    xLabel: String = "",
+    yLabel: String = "",
+    withLegend: Boolean = true
+) extends PlotOptions
+
+
+
+
+abstract class DataSeries extends Proxy {
+  override def self: JFCSeries
+}
+
+
+case class XYSeries(title: String = "") extends DataSeries {
+  val self = new jfcXY.XYSeries(title)
+
+  def addPoints(points: Iterator[(Float, Float)]) : Unit =
+    for (p <- points) self.add(p._1, p._2)
+
+  def addPoints(x: Iterator[Float], y: Iterator[Float]) : Unit =
+    addPoints(x zip y)
+
+  def addPoints(x: Iterable[Float], y: Iterable[Float]) : Unit =
+    addPoints(x.iterator, y.iterator)
+}
+
+
+
+abstract class XYPlot extends Plot {
+  override def options: XYPlotOptions
+}
+
+
+
+class XYLinePlot(val options: XYPlotOptions) extends XYPlot {
+  protected def jfcLineChart(dataset: JFCXYDataset): JFCChart = {
+    JFCChartFactory.createXYLineChart(
+      options.title, options.xLabel, options.yLabel,
+      dataset,
+      JFCPlotOrientation.VERTICAL,
+      options.withLegend,
+      true, // tooltips
+      false // urls
+    )
+  }
+
+  val jfcDataset = new jfcXY.XYSeriesCollection()
+
+  val self = jfcLineChart(jfcDataset)
+
+  def addSeries(series: XYSeries) : Unit = jfcDataset.addSeries(series.self)
+}
+
+
+object XYPlot {
+  def apply(series: XYSeries, options: XYPlotOptions) : XYPlot = {
+    val plot = new XYLinePlot(options)
+    plot.addSeries(series)
+    plot
+  }
+
+  def apply(series: XYSeries) : XYPlot = XYPlot(series, XYPlotOptions())
+
+  def apply(points: Iterator[(Float, Float)], options: XYPlotOptions = XYPlotOptions()) : XYPlot = {
+    val series = XYSeries()
+    series.addPoints(points)
+    XYPlot(series, options)
+  }
+
+  def apply(x: Iterator[Float], y: Iterator[Float], options: XYPlotOptions) : XYPlot =
+    XYPlot(x zip y, options)
+
+  def apply(x: Iterator[Float], y: Iterator[Float]) : XYPlot =
+    XYPlot(x zip y)
+
+  def apply(x: Iterable[Float], y: Iterable[Float], options: XYPlotOptions) : XYPlot =
+    XYPlot(x.iterator, y.iterator, options)
+
+  def apply(x: Iterable[Float], y: Iterable[Float]) : XYPlot =
+    XYPlot(x.iterator, y.iterator)
+}
+
+
+
+class PlotPanel(plot: Plot) extends Panel {
+  override lazy val peer = {
+    new org.jfree.chart.ChartPanel(plot.self)
+  }
+}
+
+object PlotPanel {
+  def apply(plot: Plot) = new PlotPanel(plot)
+}
+
+
+
+case class PlotFrame(plot: Plot, frameTitle: String = "Plot") extends Frame {
+  title = frameTitle
+  contents = PlotPanel(plot)
+  
+  def show() = Swing.onEDT {
+    pack()
+    visible = true
+  }
+  
+  show()
+}
