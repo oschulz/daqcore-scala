eeb1385965c0a7e8a818fa362110cd827e985e55
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-05-31 00:41:13 +0200

    Changed ByteOrder to operator on Seqs, not IndexedSeqs

--- src/main/scala/daqcore/util/ByteOrder.scala
+++ src/main/scala/daqcore/util/ByteOrder.scala
@@ -22,86 +22,86 @@ import java.nio.{ByteOrder => NIOByteOrder, ByteBuffer => NIOByteBuffer}
 sealed abstract class ByteOrder {
   def toNIO: NIOByteOrder
 
-  def fromBytes[A <: AnyVal : ClassManifest](bytes: Seq[Byte]) : IndexedSeq[A] = {
-    val byteArray = bytes.toArray
-    val byteBuf = NIOByteBuffer.wrap(byteArray)
-    if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
-
+  def fromBytes[A <: AnyVal : ClassManifest](bytes: Seq[Byte]) : Seq[A] = {
     val mf = classManifest[A]
-    if (mf == classManifest[Byte]) {
-      bytes.toIndexedSeq.asInstanceOf[IndexedSeq[A]]
+    if (mf == classManifest[Byte]) bytes.asInstanceOf[Seq[A]]
+    else {
+      val byteArray = bytes.toArray
+      val byteBuf = NIOByteBuffer.wrap(byteArray)
+      if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
+
+      if (mf == classManifest[Char]) {
+        val buf = byteBuf.asCharBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else if (mf == classManifest[Short]) {
+        val buf = byteBuf.asShortBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else if (mf == classManifest[Int]) {
+        val buf = byteBuf.asIntBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else if (mf == classManifest[Long]) {
+        val buf = byteBuf.asLongBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else if (mf == classManifest[Float]) {
+        val buf = byteBuf.asFloatBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else if (mf == classManifest[Double]) {
+        val buf = byteBuf.asDoubleBuffer
+        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      }
+      else throw new IllegalArgumentException("convertTo() does not support " + mf)
     }
-    else if (mf == classManifest[Char]) {
-      val buf = byteBuf.asCharBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else if (mf == classManifest[Short]) {
-      val buf = byteBuf.asShortBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else if (mf == classManifest[Int]) {
-      val buf = byteBuf.asIntBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else if (mf == classManifest[Long]) {
-      val buf = byteBuf.asLongBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else if (mf == classManifest[Float]) {
-      val buf = byteBuf.asFloatBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else if (mf == classManifest[Double]) {
-      val buf = byteBuf.asDoubleBuffer
-      IndexedSeq.fill(buf.limit){buf.get}.asInstanceOf[IndexedSeq[A]]
-    }
-    else throw new IllegalArgumentException("convertTo() does not support " + mf) 
   }
 
 
-  def toBytes[A <: AnyVal : ClassManifest](seq: Seq[A]) : IndexedSeq[Byte] = {
-    val byteBuf = NIOByteBuffer.allocate(seq.length * sizeOf[A])
-    if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
-    assert (byteBuf.hasArray)
-    val byteArray = byteBuf.array
-    assert (byteArray.length == seq.length * sizeOf[A])
-    
+  def toBytes[A <: AnyVal : ClassManifest](seq: Seq[A]) : Seq[Byte] = {
     val mf = classManifest[A]
-    if (mf == classManifest[Byte]) {
-      seq.toIndexedSeq.asInstanceOf[IndexedSeq[A]]
+    if (mf == classManifest[Byte]) seq.asInstanceOf[Seq[Byte]]
+    else {
+      val byteBuf = NIOByteBuffer.allocate(seq.size * sizeOf[A])
+      if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
+      assert (byteBuf.hasArray)
+      val byteArray = byteBuf.array
+      assert (byteArray.size == seq.size * sizeOf[A])
+
+      if (mf == classManifest[Char]) {
+        val buf = byteBuf.asCharBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Char]] foreach { buf.put(_) }
+      }
+      else if (mf == classManifest[Short]) {
+        val buf = byteBuf.asShortBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Short]] foreach { buf.put(_) }
+      }
+      else if (mf == classManifest[Int]) {
+        val buf = byteBuf.asIntBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Int]] foreach { buf.put(_) }
+      }
+      else if (mf == classManifest[Long]) {
+        val buf = byteBuf.asLongBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Long]] foreach { buf.put(_) }
+      }
+      else if (mf == classManifest[Float]) {
+        val buf = byteBuf.asFloatBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Float]] foreach { buf.put(_) }
+      }
+      else if (mf == classManifest[Double]) {
+        val buf = byteBuf.asDoubleBuffer
+        assert(buf.capacity == seq.size)
+        seq.asInstanceOf[Seq[Double]] foreach { buf.put(_) }
+      }
+      
+      byteArray.toSeq
     }
-    else if (mf == classManifest[Char]) {
-      val buf = byteBuf.asCharBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Char]] foreach { buf.put(_) }
-    }
-    else if (mf == classManifest[Short]) {
-      val buf = byteBuf.asShortBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Short]] foreach { buf.put(_) }
-    }
-    else if (mf == classManifest[Int]) {
-      val buf = byteBuf.asIntBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Int]] foreach { buf.put(_) }
-    }
-    else if (mf == classManifest[Long]) {
-      val buf = byteBuf.asLongBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Long]] foreach { buf.put(_) }
-    }
-    else if (mf == classManifest[Float]) {
-      val buf = byteBuf.asFloatBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Float]] foreach { buf.put(_) }
-    }
-    else if (mf == classManifest[Double]) {
-      val buf = byteBuf.asDoubleBuffer
-      assert(buf.capacity == seq.length)
-      seq.asInstanceOf[Seq[Double]] foreach { buf.put(_) }
-    }
-    
-    byteArray.toSeq.asInstanceOf[IndexedSeq[Byte]]
   }
 }
 
