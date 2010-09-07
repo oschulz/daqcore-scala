36f3569bcb1b9225b243d310240e3932c6115f3a
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-09-07 23:10:59 +0200

    Added StatefulFilter

---
+++ src/main/scala/daqcore/math/StatefulFilter.scala
@@ -0,0 +1,57 @@
+// Copyright (C) 2010 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.math
+
+import daqcore.util._
+
+
+trait StatefulFilter[A,B] extends Mutable with Function[A,B]
+
+
+// y(n) = 1/a(0) * ( (b(0)*x(n-0) + b(1) * x(n-1) + ...
+//                  - a(1) * y(n-1) - a(2) * y(n-2) - ... )
+
+case class iirFilter(a: IndexedSeq[Double], b:IndexedSeq[Double]) extends StatefulFilter[Double, Double] {
+  var initialized = false
+  var x: RingBuffer[Double] = null
+  var y: RingBuffer[Double] = null
+  
+  def apply(in: Double) = {
+    if (!initialized) {
+      x = RingBuffer(b.size, in)
+      y = RingBuffer(a.size - 1, in)
+      initialized = true
+    } else x.pushFront(in)
+    
+    var out = 0.;
+    for (i <- 0 to b.size - 1) out += b(i)*x(i)
+    for (i <- 1 to a.size - 1) out -= a(i)*y(i-1)
+    out = out / a(0)
+    
+    y.pushFront(out)
+    out
+  }
+}
+
+
+object RCFilter {
+  def apply(c: Double) = {
+    val alpha = 1. / (1. + c)
+    iirFilter(a = Vector(1., alpha-1), b = Vector(alpha))
+  }
+}
