22ef4e1fd9b29451f70d8f941cb50725d65c42a2
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-09-25 15:45:53 +0200

    Improved EventWriter SCPI handling

--- src/main/scala/daqcore/servers/EventWriter.scala
+++ src/main/scala/daqcore/servers/EventWriter.scala
@@ -33,13 +33,8 @@ import daqcore.data._
 
 class EventWriter(val source: EventSource, val output: SCPIRequestOutput) extends CloseableServer {
   import daqcore.prot.scpi._
-
-  val CHANnel = Mnemonic("CHANnel")
-  val TRANSient = Mnemonic("TRANSient")
-  val TPOSition = Mnemonic("TPOSition")
-  val NSAMples = Mnemonic("NSAMples")
-  val SAMples = Mnemonic("SAMples")
-
+  import EventWriter.headers._
+  
   val handler = EventHandler {
     case ev: RunStart => srv ! ev; true
     case ev: RunStop => srv ! ev; true
@@ -58,7 +53,7 @@ class EventWriter(val source: EventSource, val output: SCPIRequestOutput) extend
 
   {
     import daqcore.prot.scpi.mnemonics._
-    write(~FORMat!(SPD("DaqCoRE-Events-SCPI"), SPD("0.1")))
+    write(H_FORMat!(SPD("DaqCoRE-Events-SCPI"), SPD("0.1")))
   }
 
   override def init() = {
@@ -73,23 +68,23 @@ class EventWriter(val source: EventSource, val output: SCPIRequestOutput) extend
       import daqcore.prot.scpi.mnemonics._
       trace(loggable(event))
       val transCh = event.trans.keys.toSeq.sortWith{_ < _}
-      write( ~EVENt!( NR1(event.idx), NRf(event.time)) )
-      write( ~EVEN~TRIGger!(event.trig map {t => NR1(t)}: _*) )
-      write( ~EVENt~TRANSient~CHANnel!((for (ch <- transCh.view) yield NR1(ch)): _*) )
-      write( ~EVENt~TRANSient~TPOSition!((for (ch <- transCh.view) yield NR1(event.trans(ch).trigPos)): _*) )
-      write( ~EVENt~TRANSient~NSAMples!((for (ch <- transCh.view) yield NR1(event.trans(ch).samples.size)): _*) )
-      write( ~EVENt~TRANSient~SAMples! ((for {ch <- transCh.view; s <-event.trans(ch).samples.view} yield NR1(s)): _*) )
-      write( ~EVENt~END! )
+      write( H_EVENt!( NR1(event.idx), NRf(event.time)) )
+      write( H_EVEN_TRIGger!(event.trig map {t => NR1(t)}: _*) )
+      write( H_EVENt_TRANSient_CHANnel!((for (ch <- transCh.view) yield NR1(ch)): _*) )
+      write( H_EVENt_TRANSient_TPOSition!((for (ch <- transCh.view) yield NR1(event.trans(ch).trigPos)): _*) )
+      write( H_EVENt_TRANSient_NSAMples!((for (ch <- transCh.view) yield NR1(event.trans(ch).samples.size)): _*) )
+      write( H_EVENt_TRANSient_SAMples! ((for {ch <- transCh.view; s <-event.trans(ch).samples.view} yield NR1(s)): _*) )
+      write( H_EVENt_END! )
     }
     case op @ RunStart(uuid, time) => {
       import daqcore.prot.scpi.mnemonics._
       debug(op)
-      write(~RUN~STARt!(SPD(uuid.toString), NRf(time)))
+      write(H_RUN_STARt!(SPD(uuid.toString), NRf(time)))
     }
     case op @ RunStop(duration) => {
       import daqcore.prot.scpi.mnemonics._
       debug(op)
-      write(~RUN~STOP!(NRf(duration)))
+      write(H_RUN_STOP!(NRf(duration)))
       flush()
     }
 
@@ -116,6 +111,33 @@ class EventWriter(val source: EventSource, val output: SCPIRequestOutput) extend
 
 
 object EventWriter {
+  object mnemonics {
+    import daqcore.prot.scpi._
+    val CHANnel = Mnemonic("CHANnel")
+    val TRANSient = Mnemonic("TRANSient")
+    val TPOSition = Mnemonic("TPOSition")
+    val NSAMples = Mnemonic("NSAMples")
+    val SAMples = Mnemonic("SAMples")
+  }
+  
+  object headers {
+    import daqcore.prot.scpi.mnemonics._
+    import EventWriter.mnemonics._
+    
+    val H_FORMat = ~FORMat
+
+    val H_EVENt = ~EVENt
+    val H_EVEN_TRIGger = ~EVENt~TRIGger
+    val H_EVENt_TRANSient_CHANnel = ~EVENt~TRANSient~CHANnel
+    val H_EVENt_TRANSient_TPOSition = ~EVENt~TRANSient~TPOSition
+    val H_EVENt_TRANSient_NSAMples = ~EVENt~TRANSient~NSAMples
+    val H_EVENt_TRANSient_SAMples = ~EVENt~TRANSient~SAMples
+    val H_EVENt_END = ~EVENt~END
+
+    val H_RUN_STARt = ~RUN~STARt
+    val H_RUN_STOP = ~RUN~STOP
+  }
+  
   def apply(source: EventSource, output: SCPIRequestOutput): EventWriter =
     start(new EventWriter(source, output))
     
