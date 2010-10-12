e4b4de66195e50267dc1ecbefff90c775408e886
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2010-10-13 00:41:45 +0200

    Changed GPIBStreamExtractor to use GenericByteSeqExtractor

--- src/main/scala/daqcore/prot/scpi/GPIBStreamExtractor.scala
+++ src/main/scala/daqcore/prot/scpi/GPIBStreamExtractor.scala
@@ -22,17 +22,9 @@ import daqcore.util._
 import daqcore.prot._
 
 
-case class GPIBStreamExtractor() extends Logging {
-  type ParseReturn = (Boolean, Int, ParseFunction)
-
-  trait ParseFunction extends ((Array[Byte], Int) => ParseReturn)
-    { def apply(input: Array[Byte], pos: Int): ParseReturn }
-  
-  implicit def parseFunction(f: (Array[Byte], Int) => ParseReturn) =
-    new ParseFunction { def apply(input: Array[Byte], pos: Int) = f(input, pos) }
-  
-  def parseDefault(input: Array[Byte], pos: Int): ParseReturn = {
-    trace("parseDefault(%s)".format(ByteCharSeq(SubArray(input, pos, input.size).toArray)))
+case class GPIBStreamExtractor() extends GenericByteSeqExtractor {
+  protected def parseStart(input: Array[Byte], pos: Int): ParseReturn = {
+    trace("parseStart(%s)".format(ByteCharSeq(SubArray(input, pos, input.size).toArray)))
     val newline = '\n'.toByte
     val singleQuote = '"'.toByte
     val doubleQuote = '\''.toByte
@@ -44,13 +36,13 @@ case class GPIBStreamExtractor() extends Logging {
     while ((p < input.size) && (next == null)) {
       input(p) match {
         case b @ (`singleQuote` | `doubleQuote`) => next = parseQuoted(b) _
-        case b @ `newline` => { finished = true; next = parseDefault _}
+        case b @ `newline` => { finished = true; next = parseStart _}
         case b @ `hash` => next = parseHash _ 
         case _ =>
       }
       p += 1
     }
-    if (next == null) next = parseDefault _
+    if (next == null) next = parseStart _
     (finished, p, next)
   }
   
@@ -59,8 +51,8 @@ case class GPIBStreamExtractor() extends Logging {
     input(pos) match {
       case b if (b >= '1'.toByte) && (b <= '9'.toByte) =>
         (false, pos+1, parseDLBDSize(0, b.toByte.toChar.toString.toInt) _) // Definite Length Block Data
-      case b if (b == '0'.toByte) => (false, pos + 1, parseDefault _) // Arbitrary Length Block Data
-      case _ => (false, pos + 1, parseDefault _)
+      case b if (b == '0'.toByte) => (false, pos + 1, parseStart _) // Arbitrary Length Block Data
+      case _ => (false, pos + 1, parseStart _)
     }
   }
   
@@ -80,7 +72,7 @@ case class GPIBStreamExtractor() extends Logging {
   def parseDLBDContents(remLen: Int)(input: Array[Byte], pos: Int): ParseReturn = {
     trace("parseDLBDContents(%s)(%s)".format(remLen, ByteCharSeq(SubArray(input, pos, input.size).toArray)))
     if (pos + remLen > input.size) (false, input.size, parseDLBDContents(pos + remLen - input.size) _ )
-    else (false, pos + remLen, parseDefault _)
+    else (false, pos + remLen, parseStart _)
   }
   
   def parseQuoted(quoteChar: Byte)(input: Array[Byte], pos: Int): ParseReturn = {
@@ -94,52 +86,6 @@ case class GPIBStreamExtractor() extends Logging {
   def parseQuotedEsc(quoteChar: Byte)(input: Array[Byte], pos: Int): ParseReturn = {
     trace("parseQuotedEsc(%s)(%s)".format(quoteChar, ByteCharSeq(SubArray(input, pos, input.size).toArray)))
     if (input(pos) == quoteChar) (false, pos+1, parseQuoted(quoteChar) _)
-    else (false, pos, parseDefault _)
-  }
-  
-  
-  case class SubArray(array: Array[Byte], from: Int, until: Int) {
-    require ((from >= 0) && (until >= from))
-    def size = until - from
-    def toArray = array.slice(from, until)
-  }
-  
-  protected var inputQueue: List[SubArray] = Nil
-  protected var parseFunc = parseDefault _
-  
-  def unfinished: Boolean = ! inputQueue.isEmpty
-
-  def apply(bytes: Seq[Byte]): Seq[Seq[Byte]] = {
-    var extracted: List[Seq[Byte]] = Nil
-  
-    trace("apply(%s)".format(loggable(ByteCharSeq(bytes: _*))))
-
-    val array = bytes.toArray
-    var startPos = 0
-    var pos = 0
-    while (pos < array.size) {
-      val (msgComplete, nextPos, nextParseFunc) = parseFunc(array, pos)
-      pos = nextPos
-      parseFunc = nextParseFunc
-      if (msgComplete) {
-        inputQueue = SubArray(array, startPos, pos) :: inputQueue
-        val totalSize = inputQueue.view map {_.size} sum
-        val outputArray = Array.ofDim[Byte](totalSize)
-        var outputPos = 0
-        for (in <- inputQueue.reverse) {
-          for (i <- 0 to in.size - 1) outputArray(outputPos + i) = in.array(in.from + i)
-          outputPos += in.size
-        }
-        val msg = outputArray.toIISeq
-        trace("Complete message of length %s available: [%s]".format(msg.length, loggable(msg)))
-        extracted = msg :: extracted
-        startPos = pos
-        inputQueue = Nil
-      }
-    }
-    if (startPos < pos) inputQueue = SubArray(array, startPos, pos) :: inputQueue
-    // trace("inputQueue:" + loggable(inputQueue map loggable))
-    
-    extracted.reverse
+    else (false, pos, parseStart _)
   }
 }
