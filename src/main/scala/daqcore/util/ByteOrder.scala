a0e7c04f6548a7d6b2748146197dfb20d4eeac1b
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2011-01-02 00:05:33 +0100

    Changed ByteOrder implementation, switched to ByteSeq

--- src/main/scala/daqcore/util/ByteOrder.scala
+++ src/main/scala/daqcore/util/ByteOrder.scala
@@ -22,46 +22,55 @@ import java.nio.{ByteOrder => NIOByteOrder, ByteBuffer => NIOByteBuffer}
 sealed trait ByteOrder {
   def toNIO: NIOByteOrder
 
-  def fromBytes[A <: AnyVal : ClassManifest](bytes: Seq[Byte]) : Seq[A] = {
+  def fromBytes[A <: AnyVal : ClassManifest](bytes: ByteSeq) : ArrayVec[A] = {
     val mf = classManifest[A]
-    if (mf == classManifest[Byte]) bytes.asInstanceOf[Seq[A]]
+    if (mf == classManifest[Byte]) bytes.asInstanceOf[ArrayVec[A]]
     else {
       val byteArray = bytes.toArray
       val byteBuf = NIOByteBuffer.wrap(byteArray)
       if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
 
-      if (mf == classManifest[Char]) {
-        val buf = byteBuf.asCharBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+      if (mf == classManifest[Byte]) {
+        ArrayVec.wrap(bytes.toArray.asInstanceOf[Array[A]])
       }
       else if (mf == classManifest[Short]) {
         val buf = byteBuf.asShortBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+        val array = Array.ofDim[Short](buf.limit)
+        buf.get(array)
+        ArrayVec.wrap(array.asInstanceOf[Array[A]])
       }
       else if (mf == classManifest[Int]) {
         val buf = byteBuf.asIntBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+        val array = Array.ofDim[Int](buf.limit)
+        buf.get(array)
+        ArrayVec.wrap(array.asInstanceOf[Array[A]])
       }
       else if (mf == classManifest[Long]) {
         val buf = byteBuf.asLongBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+        val array = Array.ofDim[Long](buf.limit)
+        buf.get(array)
+        ArrayVec.wrap(array.asInstanceOf[Array[A]])
       }
       else if (mf == classManifest[Float]) {
         val buf = byteBuf.asFloatBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+        val array = Array.ofDim[Float](buf.limit)
+        buf.get(array)
+        ArrayVec.wrap(array.asInstanceOf[Array[A]])
       }
       else if (mf == classManifest[Double]) {
         val buf = byteBuf.asDoubleBuffer
-        Array.fill(buf.limit){buf.get}.toSeq.asInstanceOf[Seq[A]]
+        val array = Array.ofDim[Double](buf.limit)
+        buf.get(array)
+        ArrayVec.wrap(array.asInstanceOf[Array[A]])
       }
-      else throw new IllegalArgumentException("convertTo() does not support " + mf)
+      else throw new UnsupportedOperationException("convertTo() does not support " + mf)
     }
   }
 
 
-  def toBytes[A <: AnyVal : ClassManifest](seq: Seq[A]) : Seq[Byte] = {
+  def toBytes[A <: AnyVal : ClassManifest](seq: ArrayVec[A]) : ByteSeq = {
     val mf = classManifest[A]
-    if (mf == classManifest[Byte]) seq.asInstanceOf[Seq[Byte]]
+    if (mf == classManifest[Byte]) seq.asInstanceOf[ByteSeq]
     else {
       val byteBuf = NIOByteBuffer.allocate(seq.size * sizeOf[A])
       if (this == LittleEndian) byteBuf.order(NIOByteOrder.LITTLE_ENDIAN)
@@ -69,38 +78,36 @@ sealed trait ByteOrder {
       val byteArray = byteBuf.array
       assert (byteArray.size == seq.size * sizeOf[A])
 
-      if (mf == classManifest[Char]) {
-        val buf = byteBuf.asCharBuffer
-        assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Char]] foreach { buf.put(_) }
+      if (mf == classManifest[Byte]) {
+        byteBuf.put(seq.asInstanceOf[ArrayVec[Byte]].toArray)
       }
       else if (mf == classManifest[Short]) {
         val buf = byteBuf.asShortBuffer
         assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Short]] foreach { buf.put(_) }
+        buf.put(seq.asInstanceOf[ArrayVec[Short]].toArray)
       }
       else if (mf == classManifest[Int]) {
         val buf = byteBuf.asIntBuffer
         assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Int]] foreach { buf.put(_) }
+        buf.put(seq.asInstanceOf[ArrayVec[Int]].toArray)
       }
       else if (mf == classManifest[Long]) {
         val buf = byteBuf.asLongBuffer
         assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Long]] foreach { buf.put(_) }
+        buf.put(seq.asInstanceOf[ArrayVec[Long]].toArray)
       }
       else if (mf == classManifest[Float]) {
         val buf = byteBuf.asFloatBuffer
         assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Float]] foreach { buf.put(_) }
+        buf.put(seq.asInstanceOf[ArrayVec[Float]].toArray)
       }
       else if (mf == classManifest[Double]) {
         val buf = byteBuf.asDoubleBuffer
         assert(buf.capacity == seq.size)
-        seq.asInstanceOf[Seq[Double]] foreach { buf.put(_) }
+        buf.put(seq.asInstanceOf[ArrayVec[Double]].toArray)
       }
       
-      byteArray.toSeq
+      ByteSeq.wrap(byteArray)
     }
   }
 }
@@ -110,7 +117,7 @@ object ByteOrder {
   def apply(nioBO: NIOByteOrder) = nioBO match {
     case NIOByteOrder.BIG_ENDIAN => BigEndian
     case java.nio.ByteOrder.LITTLE_ENDIAN => LittleEndian
-    case _ => throw new IllegalArgumentException("Byte order %s not supported".format(nioBO))
+    case _ => throw new UnsupportedOperationException("Byte order %s not supported".format(nioBO))
   }
   
   val native = ByteOrder(NIOByteOrder.nativeOrder())
