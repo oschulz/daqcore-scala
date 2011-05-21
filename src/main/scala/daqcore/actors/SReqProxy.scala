42fa110d381a2806a1fa53f00bce6fe78968817d
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2011-05-22 01:34:00 +0200

    Added SReqProxy

---
+++ src/main/scala/daqcore/actors/SReqProxy.scala
@@ -0,0 +1,69 @@
+// Copyright (C) 2011 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.actors
+
+import akka.actor._
+
+import daqcore.util._
+
+import akka.dispatch.Future
+import java.lang.reflect.{InvocationTargetException, Method, InvocationHandler, Proxy}
+
+
+object SReqProxy {
+  case class SReqInvocationHandler(aref: ActorRef) extends InvocationHandler {
+    def invoke(proxy: AnyRef, method: Method, args: Array[AnyRef]): AnyRef = {
+      def noArgs = (args == null) || (args.length == 0)
+
+      val result: Any = method.getName match {
+        case "toString" => "%s(%s)".format(proxy.getClass.getInterfaces.head.getName, aref.toString)
+        case "equals"   => args(0) match {
+          case that_proxy if proxy eq that_proxy => true
+          case null => false
+          case thatp => try {
+            Proxy.getInvocationHandler(thatp) match {
+              case that: SReqInvocationHandler => this.aref == that.aref
+              case _ => false
+            }
+          } catch { case e: IllegalArgumentException => false }
+        }
+        case "hashCode" => aref.hashCode
+        case "srv" if noArgs => aref
+        case "stop" if noArgs => aref.stop
+        case _ => {
+          val func = Symbol(method.getName)
+          val arguments = args match { case null => Array.empty[AnyRef]; case args => args }
+          if (method.getReturnType == classOf[Unit]) {
+            aref.cmd(func, arguments: _*)
+          } else if (classOf[Future[_]].isAssignableFrom(method.getReturnType)) {
+            aref.qryF[Any](func, arguments: _*)
+          } else {
+            aref.qry[Any](func, arguments: _*)
+          }
+        }
+      }
+      result.asInstanceOf[AnyRef]
+    }
+  }
+
+  def apply[A <: ServerInterface : ClassManifest](aref: ActorRef) = {
+    val profile = classManifest[A].erasure
+    val proxy = Proxy.newProxyInstance(profile.getClassLoader, Array(profile), SReqInvocationHandler(aref))
+    proxy.asInstanceOf[A]
+  }
+}
