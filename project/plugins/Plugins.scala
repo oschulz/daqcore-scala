d3f720c81b72b75438306f32118006d148366c37
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2011-07-12 11:12:59 +0200

    Updated to akka-1.1.3

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akka_repo  = "Akka Maven Repository" at "http://akka.io/repository"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1.2"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1.3"
 }
