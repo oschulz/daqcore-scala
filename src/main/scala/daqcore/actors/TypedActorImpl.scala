507663b08712beaba1bb4d11e0da01c29adde40d
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2012-02-21 01:37:00 +0100

    Massively extended TypedActorImpl

--- src/main/scala/daqcore/actors/TypedActorImpl.scala
+++ src/main/scala/daqcore/actors/TypedActorImpl.scala
@@ -21,11 +21,97 @@ import akka.actor.{ActorSystem, ActorContext, ActorRef, ActorRefFactory}
 import akka.actor.contrib.daqcore.{TypedActor, TypedProps}
 import akka.dispatch.{Future, Promise}
 
-trait TypedActorImpl {
+import daqcore.util._
+import TypedActorTraits._
+
+
+trait TypedActorImpl extends Logging with Profiling
+  with PreStart with PostStop with PreRestart with PostRestart
+{
+  log.debug("Creating object for %s".format(selfId))
+
   implicit def dispatcher = TypedActor.dispatcher
 
   def self[A <: AnyRef]: A = TypedActor.self[A]
-  def selfRef: ActorRef = TypedActor.context.self
+  def selfContext = TypedActor.context
+  def selfRef: ActorRef = selfContext.self
+  
+  def selfStop(): Unit = selfContext.stop(selfRef)
+  
+  val selfId = "%s($s)".format(self, this.getClass)
   
   implicit def successfulPromise[A](x: A): Future[A] = Promise successful x
+
+  def init(): Unit = {
+    withCleanup
+      { log.debug("Actor %s initializing".format(selfId)) }
+      { log.debug("Actor %s cleaned up".format(selfId)) }
+  }
+
+  def preStart(): Unit = {
+    log.debug("preStart of " + selfId)
+    init()
+  }
+
+  def postRestart(reason: Throwable): Unit = {
+    log.debug("postRestart of " + selfId + ", caused by: " + reason)
+  }
+  
+  def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    log.debug("preRestart of " + selfId + ", caused by: " + reason)
+
+    try {
+      runCleanupActions()
+    } finally {
+    }
+  }
+
+  def postStop(): Unit = {
+    log.debug("postStop of " + selfId)
+
+    try {
+      runCleanupActions()
+      runShutdownActions()
+    } finally {
+    }
+  }
+  
+  
+  def atCleanup(body: => Unit): Unit = { cleanupActions = { (() => body) :: cleanupActions } }
+
+  def atShutdown(body: => Unit): Unit = { shutdownActions = { (() => body) :: shutdownActions } }
+  
+  def withCleanup (initBody: => Unit)(cleanupBody: => Unit) {
+    atCleanup { cleanupBody }
+    initBody
+  }
+
+  type SupportsClose = { def close(): Unit }
+
+  def closeAtCleanup[T <: SupportsClose](res: T): T = {
+    atCleanup { () => res.close() }
+    res
+  }
+
+
+  protected[actors] var cleanupActions: List[() => Unit] = Nil
+
+  protected[actors] def runCleanupActions(): Unit = {
+    while (cleanupActions != Nil) {
+      val action::rest = cleanupActions
+      cleanupActions = rest
+      try { action() } catch { case e => log.error(e.toString) }
+    }
+  }
+
+  protected[actors] var shutdownActions: List[() => Unit] = Nil
+
+  protected[actors] def runShutdownActions(): Unit = {
+    while (shutdownActions != Nil) {
+      val action::rest = shutdownActions
+      shutdownActions = rest
+      try { action() } catch { case e => log.error(e.toString) }
+    }
+  }
+
 }
