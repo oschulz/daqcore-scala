15863a7021880f4261a15e9557767b6bdc7581a6
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2012-02-21 02:17:28 +0100

    Added new implementation of io.GenericIO and io.InetConnection

---
+++ src/main/scala/daqcore/io/GenericIO.scala
@@ -0,0 +1,52 @@
+// Copyright (C) 2010-2012 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.io
+
+import akka.dispatch.Future
+import akka.util.{ByteString, Timeout}
+
+
+trait GenericInput[A] {
+  def recv(): Future[A]
+}
+
+
+trait GenericOutput[A] {
+  def send(data: A) : Unit
+
+  def flush() : Unit
+}
+
+
+trait GenericIO[A] extends GenericInput[A] with GenericOutput[A]
+
+
+
+trait ByteStreamInput extends GenericInput[ByteString]
+
+trait ByteStreamOutput extends GenericOutput[ByteString]
+
+trait ByteStreamIO extends GenericIO[ByteString] with ByteStreamInput with ByteStreamOutput
+
+
+
+trait RawMsgInput extends ByteStreamInput
+
+trait RawMsgOutput extends ByteStreamOutput
+
+trait RawMsgIO extends ByteStreamIO with RawMsgInput with RawMsgOutput
---
+++ src/main/scala/daqcore/io/InetConnection.scala
@@ -0,0 +1,79 @@
+// Copyright (C) 2010-2012 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.io
+
+import java.net.InetSocketAddress
+
+import akka.dispatch.{Future, Promise}
+import akka.util.{ByteString, Timeout}
+
+import daqcore.util._
+import daqcore.actors._, daqcore.actors.TypedActorTraits._
+
+
+trait InetConnection extends ByteStreamIO {
+   def recv(): Future[ByteString]
+   def send(data: ByteString) : Unit
+   def close(): Unit
+   def flush(): Unit
+}
+
+
+object InetConnection {
+  def apply(address: InetSocketAddress): InetConnection =
+    typedActorOf[InetConnection](new InetConnectionImpl(address))
+  
+  def apply(host: String, port: Int): InetConnection =
+    apply(new InetSocketAddress(host, port))
+}
+
+
+class InetConnectionImpl(address: InetSocketAddress) extends InetConnection with TypedActorImpl with Receiver  {
+  import akka.actor.{IO, IOManager}
+
+  val inputQueue = new DataActionQueue[ByteString]
+
+  var socket: IO.SocketHandle = IOManager(selfContext.system).connect(address)(selfRef)
+  atCleanup { socket.close() }
+
+  def recv(): Future[ByteString] = {
+    val result = Promise[ByteString]()
+    inputQueue pushAction { result success _ }
+    result
+  }
+  
+  def send(data: ByteString) : Unit = socket.write(data)
+  
+  def flush(): Unit = {} // Automatic flush on every send
+  
+  def close(): Unit = selfStop()
+
+  
+  def receive = {
+    case IO.Connected(socket, address) => {
+      println("Established connection to " + address)
+    }
+    case IO.Read(socket, bytes) => {
+      trace("Received: " + loggable(bytes))
+      inputQueue pushData bytes
+    }
+    case IO.Closed(socket: IO.SocketHandle, cause) => {
+      trace("Connection closed because: " + cause)
+    }
+  }
+}
