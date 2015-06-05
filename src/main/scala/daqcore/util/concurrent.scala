815304a3d37b473c2cbd03406e768ac21b67cb29
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2015-06-05 23:44:57 +0200

    Added package object daqcore.util.concurrent with Future conversions

---
+++ src/main/scala/daqcore/util/concurrent.scala
@@ -0,0 +1,46 @@
+// Copyright (C) 2015 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+package daqcore.util
+
+import scala.language.implicitConversions
+import scala.concurrent.{Future, ExecutionContext}
+
+
+package object concurrent {
+  implicit def combineFutureSeq1[T](xs: Seq[Future[T]])(implicit executor: ExecutionContext): Future[Seq[T]] =
+    Future.sequence(xs)
+
+  implicit def combineFutureSeq2[T](xs: Seq[Seq[Future[T]]])(implicit executor: ExecutionContext): Future[Seq[Seq[T]]] =
+    combineFutureSeq1(xs map combineFutureSeq1)
+
+  implicit def combineFutureSeq3[T](xs: Seq[Seq[Seq[Future[T]]]])(implicit executor: ExecutionContext): Future[Seq[Seq[Seq[T]]]] =
+    combineFutureSeq1(xs map combineFutureSeq2)
+
+
+  implicit def combineUnitFutureSeq1(xs: Seq[Future[Unit]])(implicit executor: ExecutionContext): Future[Unit] =
+    Future.sequence(xs) map { _ => {} }
+
+  implicit def combineUnitFutureSeq2(xs: Seq[Seq[Future[Unit]]])(implicit executor: ExecutionContext): Future[Unit] =
+    combineUnitFutureSeq1(xs map combineUnitFutureSeq1)
+
+  implicit def combineUnitFutureSeq3(xs: Seq[Seq[Seq[Future[Unit]]]])(implicit executor: ExecutionContext): Future[Unit] =
+    combineUnitFutureSeq1(xs map combineUnitFutureSeq2)
+
+
+  def ftconv[T](future: Future[T]): Future[T] = future
+}
