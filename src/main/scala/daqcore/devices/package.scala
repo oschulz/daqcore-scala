17811c0aa2d171642de56d46c1e6e1a7e16f7fd8
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2015-06-08 23:56:17 +0200

    Extended and improved devices.Ch and ChV

--- src/main/scala/daqcore/devices/package.scala
+++ src/main/scala/daqcore/devices/package.scala
@@ -1,4 +1,4 @@
-// Copyright (C) 2011-2013 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+// Copyright (C) 2011-2015 Oliver Schulz <oliver.schulz@tu-dortmund.de>
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -19,19 +19,45 @@ package daqcore
 
 import scala.language.implicitConversions
 
+import scala.collection.breakOut
 import scala.collection.immutable.{SortedMap, SortedSet}
+import scala.collection.generic.CanBuildFrom
+import scala.concurrent.{Future, ExecutionContext}
 
 
 package object devices {
   type ChV[A] = SortedMap[Int, A]
 
   object ChV {
-    def apply[A](values: (Int, A)*): ChV[A] = SortedMap(values: _*)
+    def apply[A](chValues: (Int, A)*): ChV[A] = SortedMap(chValues: _*)
 
-    def apply[A](v: (Seq[Int], A)): ChV[A] = apply((v._1 map {ch => (ch, v._2)}): _*)
+    def apply[A](channels: Traversable[Int], value: A): ChV[A] =
+      channels.map{channel => (channel, value)}(breakOut)
 
     def apply[A, B](xs: Seq[A])(f: PartialFunction[(Int, A), (Int, B)]): ChV[B] =
       SortedMap( xs.zipWithIndex map {_.swap} collect f :_*)
+
+    def newBuilder[A] = SortedMap.newBuilder[Int, A]
+
+    def future[A](ft: Future[Traversable[(Int, A)]])(implicit executor: ExecutionContext): Future[ChV[A]] =
+      ft map { xs => xs.map(identity)(breakOut) }
+  }
+
+
+  implicit class ChVOps[A](val chV: ChV[A]) extends AnyVal {
+    def vMap[B](f: A => B): ChV[B] = chV map { case (channel, value) => (channel, f(value)) }
+
+    def cvvMap[B](f: (Int, A) => B): ChV[B] = chV map { case (channel, value) => (channel, f(channel, value)) }
+
+    def ftVMap[B](f: A => Future[B])(implicit executor: ExecutionContext): Future[ChV[B]] = {
+      val itFt = chV.iterator.map{case (channel, oldValue) => f(oldValue) map {newValue => (channel, newValue)}}
+      Future.sequence(itFt) map { ChV.newBuilder[B].++=(_).result }
+    }
+
+    def ftCVVMap[B](f: (Int, A) => Future[B])(implicit executor: ExecutionContext): Future[ChV[B]] = {
+      val itFt = chV.iterator.map{case (channel, oldValue) => f(channel, oldValue) map {newValue => (channel, newValue)}}
+      Future.sequence(itFt) map { ChV.newBuilder[B].++=(_).result }
+    }
   }
 
 
@@ -39,7 +65,38 @@ package object devices {
 
   object Ch {
     def apply(ch: Int): Ch = SortedSet[Int](ch)
-    def apply(channels: Seq[Int]*): Ch = SortedSet[Int](channels.flatten: _*)
     def apply(ch: Int, channels: Int*): Ch = apply(channels) + ch
+    def apply(channels: Traversable[Int]*): Ch = channels.flatMap(identity){breakOut}
   }
+
+
+
+  implicit class IntChOps(val channel: Int) extends AnyVal {
+    def -->[A](value: A): ChV[A] = ChV((channel, value))
+
+    def vMap[A](f: Int => A): ChV[A] = ChV((channel, f(channel)))
+
+    def ftVMap[A](f: Int => Future[A])(implicit executor: ExecutionContext): Future[ChV[A]] =
+      f(channel) map { value => ChV((channel, value)) }
+  }
+
+
+
+  implicit class TraversableChOps(val channels: Traversable[Int]) extends AnyVal {
+    def -->[A](value: A): ChV[A] = ChV(channels, value)
+
+    def vMap[A](f: Int => A): ChV[A] = channels.map{channel => (channel, f(channel))}(breakOut)
+
+    def ftVMap[A](f: Int => Future[A])(implicit executor: ExecutionContext): Future[ChV[A]] = {
+      val itFt = channels.iterator.map{channel => f(channel) map {value => (channel, value)}}
+      Future.sequence(itFt) map { ChV.newBuilder[A].++=(_).result }
+    }
+  }
+
+
+  // Currently not possible, as it breaks the implicit Int.to (for some
+  // reason, there is also a SortedSet.to):
+  //   implicit def intToCh(channel: Int): Ch = Ch(channel)
+
+  implicit def traversableToCh(xs: Traversable[Int]): Ch = Ch(xs)
 }
